ccaf16338f73102dc59fbc0f56b4099d76c40343
Thar Lin Htet <tharlin.htet@kbzbank.com>
2024-09-02 16:43:17 +0630

    first commit

---
+++ index.mjs
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+// index.mjs
+import inquirer from "inquirer";
+import { exec } from "child_process";
+import path from "path";
+
+async function init() {
+  const answers = await inquirer.prompt([
+    {
+      type: "list",
+      name: "template",
+      message: "Which template would you like to use:",
+      choices: ["Vue", "React", "Vanilla JS"],
+      default: "React",
+    },
+    {
+      type: "input",
+      name: "projectName",
+      message: "Enter your project name:",
+      default: "mini-app",
+    },
+  ]);
+
+  const { template, projectName } = answers;
+  const targetPath = path.join(process.cwd(), projectName);
+
+  let repoUrl;
+
+  // Select the repository based on the user's choice
+  switch (template) {
+    case "React":
+      repoUrl = "https://github.com/Linn-Htet123/mini-app-template-react.git";
+      break;
+    case "Vue":
+      repoUrl = "https://github.com/your-username/vue-template.git";
+      break;
+    case "Vanilla JS":
+      repoUrl = "https://github.com/your-username/vanilla-js-template.git";
+      break;
+  }
+
+  // Clone the selected repository
+  exec(`git clone ${repoUrl} ${targetPath}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error cloning the repository: ${error.message}`);
+      return;
+    }
+    console.log(`Project created successfully in ${targetPath}`);
+  });
+}
+
+init();
